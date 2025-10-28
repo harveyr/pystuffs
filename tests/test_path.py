4c1564e84b90daf73a60d11378384185ccc8c8af
Harvey Rogers <harveyr@gmail.com>
2025-10-27 20:47:38 -0500

    fix test

--- tests/test_path.py
+++ tests/test_path.py
@@ -5,7 +5,7 @@ from pystuffs.path import unexpand_user
 
 def test_unexpand_user_with_path_instance():
     raw = "~/my/test/path.py"
-    test_path = Path.expanduser(raw)
+    test_path = Path(raw).expanduser()
     assert str(test_path) != raw
     assert str(test_path).startswith("/")
 
